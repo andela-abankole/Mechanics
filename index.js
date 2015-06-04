99404102c6c0ea2fa897842a7691445dc1ffdd36
andela-abankole <akinjide.bankole@andela.co>
2015-06-04 12:17:11 +0100

    server file

---
+++ index.js
@@ -0,0 +1,45 @@
+// index.js
+
+//BASE SETUP
+
+var mongoose      = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var Mechanic      = require('./scripts/models/mechanic.model');
+
+//call the packages we need
+var express       = require('express');       // call express
+var app           = express();                // define our app using express
+var bodyParser    = require('body-parser');
+var morgan        = require('morgan');
+
+var jwt           =
+var routes        = require('./scripts/routes');
+
+/** configure app to sue bodyParser()
+  * this will let us get the data from a POST
+  **/
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+//ROUTES FOR OUR API
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next){
+  //do logging
+  console.log('New API call');
+  next(); // make sure we go to the next routes and don't stop here
+});
+
+/** REGISTER OUR ROUTES _ _ _ _
+  * all of our routes will be prefixed with /api
+  **/
+app.use('/api', router);
+routes(router);
+
+// START THE SERVER
+
+app.listen(port);
+console.log('Magician: Magic happens on port ' + port);
