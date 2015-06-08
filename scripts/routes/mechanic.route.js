a70a8c55c543c0450be646d855a0a2d853cc163c
andela-abankole <akinjide.bankole@andela.co>
2015-06-08 11:30:00 +0100

    route file for accessing mechanics

--- scripts/routes/mechanic.route.js
+++ scripts/routes/mechanic.route.js
@@ -1,31 +1,29 @@
 var express         = require('express');
 var router          = express.Router();
-var Mechanic_cntrl  = require('../controllers/mechanic.controller');
+var mechanic_Ctrl  = require('../controllers/mechanic.controller');
 
 module.exports = function (router) {
 
   // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-  router.get('/', Mechanic_cntrl.getApi);
+  router.get('/', mechanic_Ctrl.getApi);
 
   // on routes that end in /mechanics
   router.route('/mechanics')
     // create a mechanic (accessed at POST http://localhost:8080/api/mechanics)
-    .post(Mechanic_cntrl.createMechanic)
+    .post(mechanic_Ctrl.createMechanic)
 
     //get all the mechanics (accessed at GET http://localhost:8080/api/mechanics)
-    .get(Mechanic_cntrl.getAllMechanics);
-
+    .get(mechanic_Ctrl.getAllMechanics);
 
   // on routes that end in /mechanics/:mechanic_id
   router.route('/mechanics/:mechanic_id')
     // get the mechanic with that id (accessed at GET http://localhost:8080/api/mechanics/:mechanic_id)
-    .get(Mechanic_cntrl.getMechanicById)
+    .get(mechanic_Ctrl.getMechanicById)
 
 
     // update the mechanic with this id (accessed at PUT http://localhost:8080/api/mechanics/:mechanic_id)
-    .put(Mechanic_cntrl.putMechanicById)
+    .put(mechanic_Ctrl.putMechanicById)
 
-    // delete the mechanic with this id (accessed at PUT http://localhost:8080/api/mechanics/:mechanic_id)
-    .delete(Mechanic_cntrl.deleteMechanicById);
-
+    // deletes the mechanic with this id (accessed at PUT http://localhost:8080/api/mechanics/:mechanic_id)
+    .delete(mechanic_Ctrl.deleteMechanicById);
 };
