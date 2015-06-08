85d9eb35b24a9caac820dc4a2313145ce8f55cba
andela-abankole <akinjide.bankole@andela.co>
2015-06-08 12:34:37 +0100

    updated server file

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 
 //BASE SETUP
 
-var mongoose      = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 var Mechanic      = require('./scripts/models/mechanic.model');
+var Admin         = require('./scripts/models/admin.model') // get our mongoose model
 
 //call the packages we need
 var express       = require('express');       // call express
 var app           = express();                // define our app using express
 var bodyParser    = require('body-parser');
 var morgan        = require('morgan');
+var mongoose      = require('mongoose');
 
-var jwt           =
+var config        = require('./config');      // get our config file
 var routes        = require('./scripts/routes');
+require('dotenv').load();
 
-/** configure app to sue bodyParser()
-  * this will let us get the data from a POST
-  **/
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+mongoose.connect(config.database);          // connect to database
+
+
+// configure app to sue bodyParser() this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 //ROUTES FOR OUR API
 var router = express.Router();              // get an instance of the express Router
@@ -29,8 +33,10 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next){
   //do logging
-  console.log('New API call');
-  next(); // make sure we go to the next routes and don't stop here
+  console.log('New API call', req.method, req.url);
+
+  // make sure we go to the next routes and don't stop here
+  next();
 });
 
 /** REGISTER OUR ROUTES _ _ _ _
