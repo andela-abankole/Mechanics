9237f0b3d32975b692ada8b70bad596ba98fa105
andela-abankole <akinjide.bankole@andela.co>
2015-06-08 13:40:20 +0100

    updated to work with mongolab and env

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-
-  'secret': process.env.JWT_DEV || process.env.JWT_PROD,
   'database': process.env.MONGO_DEV || process.env.MONGO_PROD,
-
+  'secret': process.env.JWT_DEV || process.env.JWT_PROD
 }
