d476f968419ef49461dc87cf2031dd931ce12697
andela-abankole <akinjide.bankole@andela.co>
2015-06-08 11:28:55 +0100

    route file for admin route and mechanic route

--- scripts/routes/index.js
+++ scripts/routes/index.js
@@ -1,5 +1,7 @@
-var mechanicRoutes    = require('./mechanic.route');
+var mechanicRoutes  = require('./mechanic.route');
+var adminRoutes     = require('./admin.route');
 
 module.exports  = function(router) {
   mechanicRoutes(router);
+  adminRoutes(router);
 };
