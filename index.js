6d955e338c64d8e377d99374733afd5efd5af4fd
andela-abankole <akinjide.bankole@andela.co>
2015-06-08 13:40:47 +0100

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 
 //BASE SETUP
-
+require('dotenv').load();
 var Mechanic      = require('./scripts/models/mechanic.model');
 var Admin         = require('./scripts/models/admin.model') // get our mongoose model
 
@@ -14,12 +14,10 @@ var mongoose      = require('mongoose');
 
 var config        = require('./config');      // get our config file
 var routes        = require('./scripts/routes');
-require('dotenv').load();
 
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 3000;        // set our port
 mongoose.connect(config.database);          // connect to database
-
 
 // configure app to sue bodyParser() this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,15 +27,6 @@ app.use(morgan('dev'));
 
 //ROUTES FOR OUR API
 var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next){
-  //do logging
-  console.log('New API call', req.method, req.url);
-
-  // make sure we go to the next routes and don't stop here
-  next();
-});
 
 /** REGISTER OUR ROUTES _ _ _ _
   * all of our routes will be prefixed with /api
