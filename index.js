3206923e16f9ef920702bb2141d65d8a13bd3386
andela-abankole <akinjide.bankole@andela.co>
2015-06-09 19:23:36 +0100

    modified

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 //BASE SETUP
 require('dotenv').load();
-var Mechanic      = require('./scripts/models/mechanic.model');
-var Admin         = require('./scripts/models/admin.model') // get our mongoose model
+var Mechanic      = require('./app/models/mechanic.model');
+var Admin         = require('./app/models/admin.model') // get our mongoose model
 
 //call the packages we need
 var express       = require('express');       // call express
@@ -13,8 +13,7 @@ var morgan        = require('morgan');
 var mongoose      = require('mongoose');
 
 var config        = require('./config');      // get our config file
-var routes        = require('./scripts/routes');
-
+var routes        = require('./app/routes');
 
 var port = process.env.PORT || 3000;        // set our port
 mongoose.connect(config.database);          // connect to database
@@ -27,6 +26,15 @@ app.use(morgan('dev'));
 
 //ROUTES FOR OUR API
 var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next){
+  //do logging
+  console.log('New API call', req.method, req.url);
+
+  // make sure we go to the next routes and don't stop here
+  next();
+});
 
 /** REGISTER OUR ROUTES _ _ _ _
   * all of our routes will be prefixed with /api
