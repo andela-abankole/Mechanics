0f9b70372a253cac75357b791cd7ac5b3599990d
andela-abankole <akinjide.bankole@andela.co>
2015-06-09 19:22:56 +0100

    moved all files from scripts to app folder

--- scripts/routes/index.js
+++
@@ -1,7 +0,0 @@
-var mechanicRoutes  = require('./mechanic.route');
-var adminRoutes     = require('./admin.route');
-
-module.exports  = function(router) {
-  mechanicRoutes(router);
-  adminRoutes(router);
-};
