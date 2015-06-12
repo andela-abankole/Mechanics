8046131a9cfe679e7e9b90cf7472c88ae02404f3
andela-abankole <akinjide.bankole@andela.co>
2015-06-12 13:31:07 +0100

    Refactor and comments

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 // index.js
 
-//BASE SETUP
+// SET UP ======================================================================
 require('dotenv').load();
-var Mechanic      = require('./app/models/mechanic.model');
-var Admin         = require('./app/models/admin.model') // get our mongoose model
+var Mechanic        = require('./app/models/mechanic.model');
+var Admin           = require('./app/models/admin.model') // get mongoose model
 
 //call the packages we need
-var express       = require('express');       // call express
-var app           = express();                // define our app using express
-var bodyParser    = require('body-parser');
-var morgan        = require('morgan');
-var mongoose      = require('mongoose');
+var express         = require('express'); // call express
+var app             = express();  // define app using express
+var bodyParser      = require('body-parser');
+var morgan          = require('morgan');
+var mongoose        = require('mongoose');
+var methodOverride  = require('method-override'); //simulate DELETE and PUT
+var config          = require('./config/config'); // get config file
+var routes          = require('./app/routes');
+var port            = process.env.PORT || 3000;  // set port
 
-var config        = require('./config');      // get our config file
-var routes        = require('./app/routes');
 
-var port = process.env.PORT || 3000;        // set our port
-mongoose.connect(config.database);          // connect to database
+// CONFIGURATION =================================================================
+mongoose.connect(config.database);   // connect to database
 
-// configure app to sue bodyParser() this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: false }));
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));                 
+
+// configure app to sue bodyParser() this will get data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));  // parse application/vnd.api+json as json
 app.use(morgan('dev'));
 
-//ROUTES FOR OUR API
-var router = express.Router();              // get an instance of the express Router
+app.use(methodOverride());
 
-// middleware to use for all requests
+
+// ROUTES =========================================================================
+var router = express.Router(); // get an instance of the express Router
+
+// middleware for all requests
 router.use(function(req, res, next){
-  //do logging
   console.log('New API call', req.method, req.url);
-
-  // make sure we go to the next routes and don't stop here
   next();
 });
 
-/** REGISTER OUR ROUTES _ _ _ _
-  * all of our routes will be prefixed with /api
-  **/
+//all routes will be prefixed with /api
 app.use('/api', router);
 routes(router);
 
-// START THE SERVER
+app.get('*', function(req, res) {
+  res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front end)
+});
 
+// START THE SERVER =================================================================
 app.listen(port);
 console.log('Magician: Magic happens on port ' + port);
