c22037e8b89c9a54c6d5615c05c93707efd79b9b
andela-abankole <akinjide.bankole@andela.co>
2015-06-16 11:58:41 +0100

    modified routes comment and rearrange routes

--- app/routes/admin.route.js
+++ app/routes/admin.route.js
@@ -7,30 +7,30 @@ var verifyToken = require('../verifyToken');
 
 module.exports  = function (router) {
 
-  // Registers a new administrator (accessed at GET http://localhost:8080/api/admin/register)
+  // Registers a new administrator (accessed at GET http://localhost:3000/api/admin/register)
   router.route('/admin/register')
     .post(Admin_Ctrl.createAdmin)
 
-  // Authenticates the user details and generates a token (accessed at GET http://localhost:8080/api/authenticate)
+  // Authenticates the user details and generates a token (accessed at GET http://localhost:3000/api/authenticate)
   router.route('/authenticate')
     .post(Admin_Ctrl.authenticate);
   
-
-  // route middleware to verify token
-  router.use(verifyToken.verify);
-  
-  // retrieves all admin (accessed at GET http://localhost:8080/api/admin)
+  // retrieves all admin (accessed at GET http://localhost:3000/api/admin)
   router.route('/admin')
     .get(Admin_Ctrl.getAllAdmin); 
 
+  router.route('/admin/:viewAdmin_id')
+    // get the admin with ID (accessed at GET http://localhost:3000/api/admin/:viewAdmin_id)
+    .get(Admin_Ctrl.getAdminById);
+
+  // route middleware to verify token
+  router.use(verifyToken.verify);
+
   // on routes that end in /mechanics/:viewAdmin_id
   router.route('/admin/:viewAdmin_id')
-    // get the admin with ID (accessed at GET http://localhost:8080/api/admin/:viewAdmin_id)
-    .get(Admin_Ctrl.getAdminById)
-
-    // updates the admin with ID (accessed at GET http://localhost:8080/api/admin/:viewAdmin_id)
+    // updates the admin with ID (accessed at GET http://localhost:3000/api/admin/:viewAdmin_id)
     .put(Admin_Ctrl.putAdminById)
 
-    // deletes the admin with ID (accessed at GET http://localhost:8080/api/admin/:viewAdmin_id)
+    // deletes the admin with ID (accessed at GET http://localhost:3000/api/admin/:viewAdmin_id)
     .delete(Admin_Ctrl.deleteAdminById);
 };
