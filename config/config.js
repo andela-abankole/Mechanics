b67e7c79bef0a0225ba9609896bac9edd950b6da
andela-abankole <akinjide.bankole@andela.co>
2015-06-16 12:09:26 +0100

    new cofig

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   'database': process.env.MONGO_DEV || process.env.MONGO_PROD,
-  'secret': process.env.JWT_DEV || process.env.JWT_PROD
+  'secret': process.env.JWT_DEV || process.env.JWT_PROD,
+  'cloudinary': process.env.CLOUDINARY_URL 
 }
