17ee84d0a93c82cab344af3fe788da4123039aec
andela-abankole <akinjide.bankole@andela.co>
2015-06-18 17:42:16 +0100

    modified

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'database': process.env.MONGO_DEV || process.env.MONGO_PROD,
   'secret': process.env.JWT_DEV || process.env.JWT_PROD,
-  'cloudinary': process.env.CLOUDINARY_URL 
+  'cloudinary': process.env.CLOUDINARY_URL
 }
