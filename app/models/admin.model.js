eedfe1b821de2d24bed0f9d599b38f14ab41a9e2
andela-abankole <akinjide.bankole@andela.co>
2015-06-19 11:50:51 +0100

    modified

--- app/models/admin.model.js
+++ app/models/admin.model.js
@@ -3,7 +3,7 @@ var mongoose        = require('mongoose');
 var Schema          = mongoose.Schema;
 var bcrypt          = require('bcrypt-nodejs');
 
-var validateEmail = function(email) {
+var validateEmail   = function(email) {
  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
  return re.test(email);
 };
