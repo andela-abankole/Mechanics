44a8cbaf4868922be87d67fc617ecd09607c19af
andela-abankole <akinjide.bankole@andela.co>
2015-06-19 11:50:28 +0100

    modified

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 var mechanicRoutes  = require('./mechanic.route');
 var adminRoutes     = require('./admin.route');
 
-module.exports  = function(router) {
+module.exports      = function(router) {
   mechanicRoutes(router);
   adminRoutes(router);
 };
