8598bf334560b03ea21b1c9f88ab9b2e5879d20f
andela-abankole <akinjide.bankole@andela.co>
2015-07-23 19:25:02 +0100

    assigned date to a variable

--- app/models/mechanic.model.js
+++ app/models/mechanic.model.js
@@ -1,11 +1,12 @@
 // app/models/model.mechanic.js
 
-var mongoose        = require('mongoose');
-var Schema          = mongoose.Schema;
-var validateEmail   = function(email) {
+var mongoose        = require('mongoose'),
+    Schema          = mongoose.Schema,
+    validateEmail   = function(email) {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
-};
+},
+    date            = Date.now();
 
 var mechanicSchema  = new Schema({
   fullname:  { 
@@ -39,11 +40,11 @@ var mechanicSchema  = new Schema({
   },
   created_at: { 
     type: Date, 
-    default: Date.now 
+    default: date
   },
   updated_at: { 
     type: Date, 
-    default: Date.now 
+    default: date
   },
 });
 
